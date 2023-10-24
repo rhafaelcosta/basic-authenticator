6c0e7a089ed41afd038d1f371f6bd3d8968b7257
Rhafael Freitas da Costa <rhafael.costa@lactec.org.br>
2023-10-24 08:08:30 -0300

    Adicionado estrutura básica do projeto

---
+++ basic-authenticator/src/main/java/com/github/rhafaelcosta/basicauthenticator/BasicAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.rhafaelcosta.basicauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ basic-authenticator/src/test/java/com/github/rhafaelcosta/basicauthenticator/BasicAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rhafaelcosta.basicauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
